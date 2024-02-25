9f3f0945606127dd9b3b7c53d3fdc9c8ab22afdc
Rushda-c <rushdac771@gmail.com>
2024-02-25 12:43:04 +0530

    Offers

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -11,7 +11,7 @@ function Offers() {
 
         </div>
         <div className='offers-right'>
-            <img src="https://plus.unsplash.com/premium_photo-1682095661711-f5d67d0e75a9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" height={200} width={200}></img>
+            <img src="https://plus.unsplash.com/premium_photo-1682095661711-f5d67d0e75a9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" height={300} width={300}></img>
 
         </div>
     </div>
